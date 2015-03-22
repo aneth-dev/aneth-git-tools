9681a806d8e5ba36966d38968a648e369fe1aaed
Thomas Pérennou <thomas.perennou@aeten.net>
2015-03-22 23:23:16 +0100

    API review

--- aeten-submodules.sh
+++ aeten-submodules.sh
@@ -6,27 +6,91 @@
 #	url = <url>
 #	branch = <branch-name>
 #	sparse-checkout = <path/to/foo>\n \
-#	                  <path/to/bar>
+#							<path/to/bar>
 
-#@@ATEN-CLI-INCLUDE@@
+#@@AETEN-CLI-INCLUDE@@
 
 __api() {
 	sed --quiet --regexp-extended 's/^(git-[[:alnum:]_-]+)\s*\(\)\s*\{/\1/p' "${*}" 2>/dev/null
 }
 
+__usage() {
+	local errno
+	errno=${1}
+	shift
+	fatal --errno ${errno} "${@}"
+	exit ${errno}
+}
+
 # Parameters: submodule
-git-submodule-revision() {
+__check-submodule-name() {
+	[ 1 -eq ${#} ] || { __usage 1 "Usage: ${FUNCNAME} submodule"; }
+	submodule=$(git submodule status "${1}" 2>/dev/null | awk '{print $2}')
+	[ -z "${submodule}" ] && { echo No submodule named ${1} >&2 ; exit 1; }
+	echo ${submodule}
+}
+
+# Parameters: submodule
+git-submodule-check() {
+	local submodules
+	local submodule
+	local error
+	if [ -z "${*}" ]; then
+		submodules=$(git submodule status|awk '{print $2}')
+	else
+		submodules=${*}
+	fi
+
+	let error=0
+	for submodule in ${submodules}; do
+		check -m "Check submodule ${1}" test "$(git-submodule-checked-out ${1})" = "$(git-submodule-show-rev ${1})" || let error+=1
+	done
+	[ ${error} -eq 0 ] || fatal One or more submodule out of sync
+}
+
+# Parameters: submodule
+git-submodule-checked-out() {
+	[ 1 -eq ${#} ] || { echo Usage: ${FUNCNAME} submodule >&2 ; exit 1; }
+	git --git-dir=.git/modules/${1} rev-parse HEAD
+}
+
+# Parameters: submodule
+git-submodule-show-rev() {
 	[ 1 -eq ${#} ] || { echo Usage: ${FUNCNAME} submodule >&2 ; exit 1; }
 	git ls-files --error-unmatch --stage -- ${1} | awk '{print $2}'
 }
 
-# Parameters: submodule branch revision
-git-submodule-revision-update-shallow() {
-	[ 3 -eq ${#} ] || { echo Usage: ${FUNCNAME} submodule branch revision >&2 ; exit 1; }
-	submodule=$(check-submodule-name ${1})
-	[ 0 -eq ${?} ] || exit ${?}
-	branch=${2}
-	revision=${3}
+# Parameters: --name <submodule> --branch <branch> --revision <revision>
+__submodule-reset-shallow() {
+	local usage
+	local branch
+	local revision
+	local name
+	local submodule
+	local arg
+	local depth
+	local sparse_checkout
+	local git_directory
+	local url
+	usage="${FUNCNAME} --name <module> --branch <branch> --revision <revision>"
+	while test ${#} -ne 0; do
+		case "${1}" in
+			--branch) branch=${2} ; shift ;;
+			--branch=*) branch=${1/--branch=/};;
+			--revision) revision=${2} ; shift ;;
+			--revision=*) revision=${1/--revision=/};;
+			--name) name=${2}; shift; ;;
+			--name=*) name=${1/--name=/};;
+			*) __usage 1 Usage: ${usage};;
+		esac
+		shift
+	done
+	for arg in name branch revision; do
+		[ -z "${!arg}" ] && __usage 1 ${usage}
+	done
+	submodule=$(__check-submodule-name ${name})
+	[ 0 -eq ${?} ] || fatal --errno ${?} "invalid module name ${name}"
+
 	let depth=1
 	url=$(git config --file=.gitmodules --get submodule.${submodule}.url)
 	sparse_checkout=$(git config --file=.gitmodules --get submodule.${submodule}.sparse-checkout | sed --regexp-extended 's/(^\s+)|(\s$)//')
@@ -50,26 +114,39 @@ git-submodule-revision-update-shallow() {
 	)
 }
 
-# Parameters: [--init] [--branch <branch>] -- [submodule1 [submodule2 [...]]]
-git-submodule-update-shallow() {
-	usage() { echo "${FUNCNAME} [--init] [--branch <branch>] -- [submodule1 [submodule2 [...]]]" >&2 ; exit 1; }
+# Parameters: [--help] [--init] [--branch <branch>] -- [submodule1 [submodule2 [...]]]
+git-submodule-reset-shallow() {
+	local usage
+	local init
+	local branch
+	local revision
+	usage="\t${FUNCNAME} [--help]
+\t${FUNCNAME} [--init] [--branch <branch>] -- [<submodule1> [<submodule2> [...]]]
+\t${FUNCNAME} --branch <branch> --revision <revision> [--] <submodule>"
 	let init=0
 	while test ${#} -ne 0; do
 		case "${1}" in
-			--init) let init=1 ;;
-			--branch) branch=${2} ; shift ;;
-			--branch=*) branch=${1/--branch=/} ;;
-			--) shift; break ;;
-			-*) usage ;;
-			*) break ;;
+			--init|-i) let init=1;;
+			--branch|-b) branch=${2} ; shift;;
+			--branch=*) branch=${1/--branch=/};;
+			--revision|r) revision=${2} ; shift;;
+			--revision=*) revision=${1/--revision=/};;
+			--help) __usage 0 ${usage};;
+			--) shift; break;;
+			-*) __usage 1 "Usage:\n${usage}";;
+			*) break;;
 		esac
 		shift
 	done
 	unset -f usage
 
+	if [ ! -z "${revision}" ] && [ 1 -ne ${#} ]; then
+		__usage 1 "Usage:\n${usage}"
+	fi
+
 	# Check all modules before run update
 	for submodule in ${*}; do
-		check -m "Check submodule name ${submodule}" check-submodule-name ${submodule}
+		__check-submodule-name "${submodule}" >/dev/null || fatal "Check submodule name ${submodule}"
 	done
 
 	if [ -z "${*}" ]; then
@@ -80,43 +157,24 @@ git-submodule-update-shallow() {
 	fi
 
 	for submodule in ${submodules}; do
-		submodule=$(check-submodule-name ${submodule})
+		submodule=$(__check-submodule-name ${submodule})
 		[ 1 -eq $init ] && check -m "Initialize submodule ${submodule}" git submodule init ${submodule}
 		if [ -z "${branch}" ]; then
 			branch=$(git config --file=.gitmodules --get submodule.${submodule}.branch)
-			revision=$(git-submodule-revision ${submodule})
+			[ -z "${revision}" ] && revision=$(git-submodule-show-rev ${submodule})
 		else
-			revision=HEAD
+			[ -z "${revision}" ] && revision=HEAD
 		fi
-		git-submodule-revision-update-shallow ${submodule} ${branch} ${revision}
+		__submodule-reset-shallow --name ${submodule} --branch ${branch} --revision ${revision}
 	done
 }
 
-# Parameters: submodule
-check-submodule-name() {
-	[ 1 -eq ${#} ] || { echo Usage: ${FUNCNAME} submodule >&2 ; exit 1; }
-	submodule=$(git submodule status "${1}" 2>/dev/null | awk '{print $2}')
-	[ -z "${submodule}" ] && { echo No submodule named ${1} >&2 ; exit 1; }
-	echo ${submodule}
-}
-
-install() {
-	[ 1 -eq ${#} ] || { echo Usage: ${FUNCNAME} install-directory >&2 ; exit 1; }
-	install_directory=${1}
-	confirm Want you really install Git commands into \"${install_directory}'" ?' || exit 0
-
-	for command in $(sed --quiet --regexp-extended 's/^(git-[[:alnum:]_-]+)\s*\(\)\s*\{/\1/p' $0); do
-		check -m "Install Git command ${command}" \
-			ln -s $(readlink -f ${0}) ${install_directory}/${command}
-	done
-}
-
-if [ -L "${0}" ] && [ 1 -eq $(__api "${0}"|grep "$(basename ${0})"|wc -l) ]; then
-   $(basename ${0}) "${@}"
+if [ -L "${0}" ] && [ 1 -eq $(__api "${0}"|grep "^$(basename ${0})$"|wc -l) ]; then
+	$(basename ${0}) "${@}"
 elif [ ! -L "${0}" ]; then
-   cmd=${1}
-   if [ 1 -eq $(__api "${0}"|grep "${cmd}"|wc -l) ]; then
-      shift
-      ${cmd} "${@}"
-   fi
+	cmd=${1}
+	if [ 1 -eq $(__api "${0}"|grep "^${cmd}$"|wc -l) ]; then
+		shift
+		${cmd} "${@}"
+	fi
 fi
--- makefile
+++ makefile
@@ -13,35 +13,42 @@ CUR_DIR = $(shell readlink -f "$(CURDIR)")
 AETEN_CLI = \#@@AETEN-CLI-INCLUDE@@
 AETEN_CLI_SCRIPT = $(AETEN_CLI_DIR)/aeten-cli.sh
 
-check = @$(AETEN_CLI_SCRIPT) check
+check = $(AETEN_CLI_SCRIPT) check
+
+ifeq ($(LIB_DIR),$(CUR_DIR))
+GITIGNORE = .gitignore
+endif
 
 .PHONY: all install uninstall
-all: .gitignore
+all:
 
 %.sh: %.sh.template
 
 .gitignore: $(SCRIPT)
-	$(check) -m 'Update .gitignore' "echo .gitignore > .gitignore && sed --quiet --regexp-extended 's/^(git-[[:alnum:]_-]+)\s*\(\)\s*\{/\1/p' $< >> .gitignore"
+	@$(check) -m 'Update .gitignore' "echo .gitignore > .gitignore && sed --quiet --regexp-extended 's/^(git-[[:alnum:]_-]+)\s*\(\)\s*\{/\1/p' $< >> .gitignore"
 
-install: $(LINKS)
+install: $(LINKS) $(GITIGNORE)
 
 uninstall:
-	$(check) -m 'Uninstall lib' rm -f $(filter-out $(CUR_DIR)/$(SCRIPT),$(LIB_DIR)/$(SCRIPT))
-	$(check) -m 'Uninstall symlinks' rm -f $(LINKS)
+	@test "$(CUR_DIR)" = "$(LIB_DIR)" || $(check) -m 'Uninstall lib' rm -f $(LIB_DIR)/$(SCRIPT)
+	@$(check) -m 'Uninstall symlinks' rm -f $(LINKS)
 
 clean:
-	$(check) -m 'Remove .gitignore' rm -f .gitignore
+	@test -f .gitignore && $(check) -m 'Remove .gitignore' rm -f .gitignore
 
 ifneq ($(LIB_DIR),$(CUR_DIR)) # Prevent circular dependency
 $(LIB_DIR)/%: %
 ifeq (true,$(include_cli))
-	$(check) -m 'Check submodule checkout' test -f $(AETEN_CLI_SCRIPT)
-	$(check) -m 'Install lib $@ with aeten-cli inclusion' "sed -e '/$(AETEN_CLI)/r $(AETEN_CLI_SCRIPT)' -e 's/$(AETEN_CLI)/let AETEN_CLI_INCLUDE=1/' -e '/^#!\/bin\/sh/d' $< > $@"
-else
-	$(check) -m 'Install lib $@' cp $< $@
+	@$(check) -m 'Check submodule checkout' test -f $(AETEN_CLI_SCRIPT)
+ifeq (./aeten-cli,$(AETEN_CLI_DIR))
+	@$(check) -l warn -m "Check submodule checkout revision" ./$(SCRIPT) git-submodule-check aeten-cli
 endif
-	$(check) -m 'Set exec flag to $@' chmod a+rx $@
+	@$(check) -m 'Install lib $@ with aeten-cli inclusion' "sed -e '/$(AETEN_CLI)/r $(AETEN_CLI_SCRIPT)' -e 's/$(AETEN_CLI)/let AETEN_CLI_INCLUDE=1/' -e '/^#!\/bin\/sh/d' $< > $@"
+else
+	@$(check) -m 'Install lib $@' cp $< $@
+endif
+	@$(check) -m 'Set exec flag to $@' chmod a+rx $@
 endif
 
 $(LINKS): $(LIB_DIR)/$(SCRIPT)
-	$(check) -m 'Install symlink $@' ln -s $< $@
+	@$(check) -m 'Install symlink $@' ln -s $< $@
